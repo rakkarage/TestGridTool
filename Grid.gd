82d18db8ed7c0f415bec01c33ec37940b4b9b8ed
Rakka Rage <rakkarage@gmail.com>
2023-09-24 14:08:21 -0400

    first

---
+++ Grid.gd
@@ -0,0 +1,12 @@
+@tool
+extends Control
+
+@export var _cell_size := 16
+
+func _draw() -> void:
+	var c := Color(1, 1, 1, 0.5)
+	var s := get_viewport_rect().size
+	for y in range(0, s.y, _cell_size):
+		draw_line(Vector2(0, y), Vector2(s.x, y), c)
+	for x in range(0, s.x, _cell_size):
+		draw_line(Vector2(x, 0), Vector2(x, s.y), c)
