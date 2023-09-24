05ba58eed17cf33ed007751e84ff2cc4810b7539
Rakka Rage <rakkarage@gmail.com>
2023-09-24 15:06:17 -0400

    x

--- GridNode2D.gd
+++ GridNode2D.gd
@@ -1,12 +1,12 @@
 @tool
-extends Control
+extends Node2D
 
 @export var _cell_size := 16
 
 func _draw() -> void:
 	var c := Color(1, 1, 1, 0.5)
-	var s := get_viewport_rect().size
-	# var s := DisplayServer.window_get_size()
+	# var s := get_viewport_rect().size
+	var s := DisplayServer.window_get_size()
 	for y in range(0, s.y, _cell_size):
 		draw_line(Vector2(0, y), Vector2(s.x, y), c)
 	for x in range(0, s.x, _cell_size):
